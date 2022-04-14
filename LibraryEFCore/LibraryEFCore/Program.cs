2774ce080ebba20c11ecdbf8ad215508a8a0668c
Goian Mădălina <goianmadalina@gmail.com>
2022-04-14 19:04:53 +0300

    created book

---
+++ LibraryEFCore/LibraryEFCore/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LibraryEFCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
